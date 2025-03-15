a652cf50a7498be9f9bc1aabdaf2d228562ff92a
Tek Bam <>
2025-03-15 18:18:29 +1100

    Change vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   root: './client',
   plugins: [react()],
   base: '/',
   build: {
     outDir: '../dist', // Place the build output in the root 'dist' folder
     emptyOutDir:true,
-  },
-  
-
-})
+  }
+  })
