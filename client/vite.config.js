a6dbd17f5c39378a03ea035b34c44b2e7fdf4940
Tek Bam <>
2025-03-15 18:26:37 +1100

    Change config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  root: './client',
+  root: '../client',
   plugins: [react()],
   base: '/',
   build: {
